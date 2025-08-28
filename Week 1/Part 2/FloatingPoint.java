a51a8aa24cc90b4064561a0a4aa9a6c7a874f8ed
Tsinan <tsinan.arkan.tif24@polban.ac.id>
2025-08-29 02:19:58 +0700

    Add files via upload

---
+++ Week 1/Part 2/FloatingPoint.java
@@ -0,0 +1,8 @@
+class FloatingPoint{
+	public static void main(String[] args) {
+		double x = 92.98;
+		long gx = (long) Math.round(x);
+		int nx = (int) Math.round(x);
+		System.out.println(nx);
+	}
+}
